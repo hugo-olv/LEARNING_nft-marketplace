62b4c959b1ad3a83ce9354380f5db4fc9f2fd0b6
themaskedcat <oliveira.developpement@gmail.com>
2022-03-21 07:51:38 +0100

    Upgrade test output*

--- smart-contract/test/sample-test.js
+++ smart-contract/test/sample-test.js
@@ -20,9 +20,11 @@ describe("NFTMarket", function () {
 
     await nft.createToken('https://media0.giphy.com/media/duzpaTbCUy9Vu/giphy.gif?cid=ecf05e470tj7pgrjk4dellfnm8redpys545p4wkeqczgr60w&rid=giphy.gif&ct=g')
     await nft.createToken('https://media4.giphy.com/media/26gsad5RsZVhKsUDe/giphy.gif?cid=ecf05e470tj7pgrjk4dellfnm8redpys545p4wkeqczgr60w&rid=giphy.gif&ct=g')
+    await nft.createToken('https://media4.giphy.com/media/26gsad5RsZVhKsUDe/giphy.gif?cid=ecf05e470tj7pgrjk4dellfnm8redpys545p4wkeqczgr60w&rid=giphy.gif&ct=g')
 
     await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
     await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+    await market.createMarketItem(nftContractAddress, 3, auctionPrice, { value: listingPrice })
 
     const [_, buyerAdress] = await ethers.getSigners()
 
@@ -32,7 +34,18 @@ describe("NFTMarket", function () {
 
     const items = await market.fetchUnsoldMarketItems()
 
-    console.log('unsold market items', items)
+    const formattedItems = await Promise.all(items.map(async ({ price, tokenId, seller, owner }) => {
+      const tokenURI = await nft.tokenURI(tokenId)
+      return {
+        price: price.toString(),
+        tokenId: tokenId.toString(),
+        seller,
+        owner,
+        tokenURI
+      }
+    }))
+
+    console.log('unsold market items', formattedItems)
 
   });
 });
